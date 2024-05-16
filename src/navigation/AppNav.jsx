e25a0f814b692ba3738af40f0ab7656c1502e697
WenThing <wenthingteo2@gmail.com>
2024-05-17 00:39:58 +0800

    init1

---
+++ src/navigation/AppNav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './Home';
+// import About from './About';
+
+const AppNav = () => {
+  return (
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //   </Routes>
+    // </Router>
+    <div>
+      <h1>AppNav</h1>
+    </div>
+  );
+};
+
+export default AppNav;
