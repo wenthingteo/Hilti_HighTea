6d489e5cab252f35ac098830c0638b1bbef282e4
WenThing <wenthingteo2@gmail.com>
2024-05-17 00:50:12 +0800

    init2

--- src/navigation/AppNav.jsx
+++ src/navigation/AppNav.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Home from './Home';
 // import About from './About';
+import PersonaHome from '../pages/ai_persona/persona_home';
+import MockivHome from '../pages/mock_interview/mockiv_home';
 
 const AppNav = () => {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/about" element={<About />} />
-    //   </Routes>
-    // </Router>
-    <div>
-      <h1>AppNav</h1>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} /> */}
+        <Route path="/ai_persona" element={<PersonaHome />} />
+        <Route path="/mock_interview" element={<MockivHome />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/pages/ai_persona/persona_home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PersonaHome() {
+    return (
+        <div>
+            <h1>persona_home</h1>
+        </div>
+    );
+}
