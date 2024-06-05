a4bbdd984dff0aabc69bce27784cff3e076d2897
WenThing <wenthingteo2@gmail.com>
2024-06-05 16:04:23 +0800

    modify ui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f3021ef33f4a63f"; }
+/******/ 		__webpack_require__.h = function() { return "d05d038a2ed564bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
